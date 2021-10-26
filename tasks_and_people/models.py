7900e660c88ab76ceeca048db8908e90e918e80c
LiorFradin <liorfradin@gmail.com>
2021-10-27 01:05:34 +0300

    Update models.py

--- tasks_and_people/models.py
+++ tasks_and_people/models.py
@@ -2,19 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Person(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField(max_length=20, primary_key=True) # I assume system id, not necessarily id of length 9
     name = models.CharField(max_length=20)
-    phoneNumber = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+    favoriteProgrammingLanguage = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "Person-> id:%s, name:%s " % (self.id, self.name)
 
 class Task(models.Model):
-    id = models.IntegerField(primary_key=True) #TODO check if indeed primary field
-    isDone = models.BooleanField(default=False)
-    description = models.CharField(max_length=200)
+    id = models.CharField(max_length=20, primary_key=True)
+    # instead of ownerId, here we save reference to the whole object
+    # in case of owners deletion it automatically deletes all his tasks
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+    isDone = models.BooleanField(default=False) #instead of status: active/done
+    details = models.CharField(max_length=200)
     dueDate = models.DateField()
+
+    def __str__(self):
+        return "Task-> id:%s, owner name:%s " % (self.id, self.owner.name) #TODO be more descriptive
 
 # Create your models here.
 # TODO remove this user, not integrated well with django
-#class User(models.Model):
+#class User(mo2dels.Model):
 #    username = models.CharField(max_length=20, primary_key=True)
 #    password = models.CharField(max_length=20)
 #    balance = models.IntegerField()
