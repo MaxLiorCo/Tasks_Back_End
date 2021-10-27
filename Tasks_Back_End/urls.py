44ccf43b678f37877adf0e1e7131c30159854fcb
LiorFradin <liorfradin@gmail.com>
2021-10-27 11:53:31 +0300

    new commit

--- Tasks_Back_End/urls.py
+++ Tasks_Back_End/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from tasks_and_people import views
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('people/', views.manage_users),
-    path('people/<int:id>', views.get_person),
+    path('api/', include('tasks_and_people.urls')), #TODO create file urls
+    path('', TemplateView.as_view(template_name='index.html')), #test
 ]
--- tasks_and_people/views.py
+++ tasks_and_people/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from django.http import HttpResponse
+from .models import *
+import json
+from django.contrib.auth.models import User
 
 @require_http_methods(["GET", "POST"])
 def manage_users(request):
     if request.method == 'GET':
-        # TODO
-    elif request.method == 'SET':
-        # TODO
+        resposnse = Person.objects.all()
+        #return JsonResponse(resposnse)
+        return HttpResponse(resposnse)
+
+    elif request.method == 'POST':
+        jsonDict = json.loads(request.POST)
+        return HttpResponse("done")
 
 
 @require_http_methods(["GET"])
 def get_person(request):
     # TODO this
+    x = 3
+
 
