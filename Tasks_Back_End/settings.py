2399e56292621ffaf64c4e457df30e5275b1949f
madMax342 <maximgel@post.bgu.ac.il>
2021-10-27 16:06:17 +0300

    CORS, views
    - Added some sort of CORS support.

--- Tasks_Back_End/settings.py
+++ Tasks_Back_End/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks_and_people.apps.TasksAndPeopleConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://reqbin.com",
+    "http://mbarsinai.com",
+    "http://localhost:9000",
 ]
 
 #ROOT_URLCONF = 'tasks_and_people.urls'
--- tasks_and_people/views.py
+++ tasks_and_people/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from tasks_and_people.models import Person, Task
+from django.core.serializers import serialize
+from django.http import JsonResponse, HttpResponse
+
 
 @require_http_methods(["GET", "POST"])
 def manage_users(request):
     if request.method == 'GET':
-        all_entries = Person.objects.all()
-        return all_entries.values()         # JSON representation
+        all_queries = Person.objects.all()
+        data = serialize("json", all_queries)   # JSON representation
+        return JsonResponse(data, safe=False)
     elif request.method == 'POST':
         request_body = request.body()
         p = Person(id=request_body.id, name=request_body.name, email=request_body.email,
                    favoriteProgrammingLanguage=request_body.favoriteProgrammingLanguage)
         p.save()
+        return HttpResponse('blyat')
 
 
 @require_http_methods(["GET", "DELETE"])
 def get_or_delete_person(request):
-    user_id = request.GET.get('id')  # I HAVE NO CLUE IF THIS WILL WORK
+    user_id = request.GET.get('id')             # I HAVE NO CLUE IF THIS WILL WORK
+    print(user_id)
     if request.method == 'GET':
-        return Person.objects.get(id=user_id)   # Should return the person that was requested
+        return serialize('json', Person.objects.get(user_id))   # Should return the person that was requested
     elif request.method == 'DELETE':
-        Person.objects.get(id=user_id).delete()
+        Person.objects.get(user_id).delete()
 
 
 @require_http_methods(["GET", "POST"])
