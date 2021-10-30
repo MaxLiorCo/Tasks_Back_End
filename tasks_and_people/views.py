8b6eec82406e55722aadcced1e989029e2ad2ee2
madMax342 <maximgel@post.bgu.ac.il>
2021-10-30 14:22:07 +0300

    Update views.py
    Function "set_task_owner" finished.

--- tasks_and_people/views.py
+++ tasks_and_people/views.py
@@ -40,7 +40,10 @@ def manage_users(request):
             if 'UNIQUE constraint' in str(e.args):
                 return HttpResponse('A person with this id already exists!', status=400)
         else:
-            return HttpResponse('New person was created successfully.', status=201)
+            resp = HttpResponse('New person was created successfully.', status=201)
+            resp["Location"] = "http://127.0.0.1:9000/api/people/{0}".format(p.id)
+            resp["x-Created-Id"] = p.id
+            return resp
 
 
 @require_http_methods(["GET", "DELETE"])
@@ -70,30 +73,76 @@ def manage_tasks(request, id):
     """
     http://127.0.0.1:9000/api/people/3/tasks?status=<some_status>.
     With "request.GET.get("status")" we can extract the status (it is a query).
-    If it is present then blah = <some_status> (probably a string), otherwise blah = None.
+    If it is present then blah = <some_status> (string), otherwise blah = None.
     With this we know if we have to filter again or not.
     """
-    blah = request.GET.get("status")
-    print(blah)
-    tmp_str = request.body.decode('utf-8')
-    received_status = False
-    status = False
-    if "status" in tmp_str:
-        body = json.loads(tmp_str)
-        status = False if body["status"] == "active" else True
-        received_status = True
     if request.method == 'GET':
+        status = request.GET.get("status")
         queries = Task.objects.filter(owner=p)
-        queries = queries if not received_status else queries.filter(isDone=status)
+        queries = queries if status is None else queries.filter(isDone=True if status == "done" else False)
         return JsonResponse(serialize("json", queries), safe=False)
     elif request.method == 'POST':
-        # TODO
-        None
+        tmp_str = request.body.decode('utf-8')
+        json_body = json.loads(tmp_str)
+        received_status = "status" in json_body
+        is_done = False if not received_status else json_body["status"] == "done"        # New task status set to active by default.
+        try:
+            t = Task(id=json_body["id"],
+                     owner=p,
+                     isDone=is_done,
+                     details=json_body["details"],
+                     dueDate=json_body["dueDate"]
+                     )
+            t.save()
+        except (KeyError, json.decoder.JSONDecodeError):
+            return HttpResponse('Required data fields are missing, data makes no sense, or data contains illegal '
+                                'values.',
+                                status=400)
+        else:
+            resp = HttpResponse("Task created and assigned successfully",
+                                status=201)
+            resp["Location"] = "http://127.0.0.1:9000/api/people/{0}/tasks".format(id)
+            resp["x-Created-Id"] = "{0}".format(t.id)
+            return resp
 
-@require_http_methods(["PUT"])
+
+@require_http_methods(["GET", "PUT"])
 @csrf_exempt
-def set_task_owner(request):
-    return None
+def set_task_owner(request, id):
+    if request.method == "GET":
+        try:
+            t = Task.objects.get(id=id)
+            p = Person.objects.get(id=t.owner.id)
+            return HttpResponse("Owner's id: {0}".format(p.id))
+        except Task.DoesNotExist:
+            return HttpResponse("Task with id: {0} does not exist.".format(id),
+                                status=404
+                                )
+    elif request.method == "PUT":
+        try:
+            json_body = json.loads(request.body.decode('utf-8'))
+            p = Person.objects.get(id=json_body["id"])
+            t = Task.objects.get(id=id)
+            t.owner = p
+            t.save(update_fields=["owner"])
+            return HttpResponse("Task owner updated successfully.", status=204)
+        except Person.DoesNotExist:
+            return HttpResponse("Person with id: {0} does not exist.".format(json_body["id"]),
+                                status=404
+                                )
+        except Task.DoesNotExist:
+            return HttpResponse("Task with id: {0} does not exist.".format(id),
+                                status=404
+                                )
+        except (KeyError, json.decoder.JSONDecodeError):
+            return HttpResponse('Required data fields are missing, data makes no sense, or data contains illegal '
+                                'values.',
+                                status=400)
+
+
+
+
+
 
 @require_http_methods(["PUT"])
 @csrf_exempt
