cbc9ca4eca3240b3591888e6b9446944d1f2e196
LiorFradin <liorfradin@gmail.com>
2021-10-31 20:10:21 +0200

    documentation

--- tasks_and_people/admin.py
+++ tasks_and_people/admin.py
@@ -2,5 +2,6 @@
 from django.contrib import admin
 from .models import Person,Task
 
+# Monitor the following models
 admin.site.register(Person)
 admin.site.register(Task)
--- tasks_and_people/models.py
+++ tasks_and_people/models.py
@@ -1,29 +1,30 @@
 from django.db import models
+# __str__ method allows us to describe how the model is printed to the user, here it is overwritten
 
-
+"""
+Person class that manages the Person model interactions in the code and its table in the database.
+-We don't need an id field since it is automatically generated
+-Email must be unique
+"""
 class Person(models.Model):
-    # We don't need an id field since it is automatically generated
-    #id = models.CharField(max_length=20, primary_key=True)  # I assume system id, not necessarily id of length 9
     name = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
+    email = models.CharField(max_length=30, unique=True)
     favoriteProgrammingLanguage = models.CharField(max_length=20)
     activeTaskCount = models.IntegerField(default=0)
 
     def __str__(self):
         return "Person-> id:%s, name:%s " % (self.id, self.name)
 
-
+"""
+Task class that manages the Task model interactions in the code and its table in the database.
+-We don't need an id field since it is automatically generated
+-Instead of ownerId, here we save a reference to the whole object.
+ In case of owners deletion it automatically deletes all his of tasks
+ ForeignKey allows us to create Many-To-One relationships
+"""
 class Task(models.Model):
-    # We don't need an id field since it is automatically generated
-    #id = models.CharField(max_length=20, primary_key=True)
-
     title = models.CharField(max_length=20, default='')
-
-    # instead of ownerId, here we save reference to the whole object
-    # in case of owners deletion it automatically deletes all his tasks
-    # ForeignKey allows us to create Many-To-One relationships
     owner = models.ForeignKey(Person, on_delete=models.CASCADE)
-
     isDone = models.BooleanField(default=False)  # instead of status: active/done
     details = models.CharField(max_length=200)
     dueDate = models.DateField()
